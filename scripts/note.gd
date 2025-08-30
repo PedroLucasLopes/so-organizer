3775bfeaba022c37ca6082cb7dbb022baf75957b
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2025-08-30 17:30:51 -0300

    folder and difference between drag and clik

--- scripts/note.gd
+++ scripts/note.gd
@@ -1,6 +1,4 @@
-class_name SOIcon
-extends Node2D
-
+class_name SOIcon extends Node2D
 
 signal attempt_to_snap
 
@@ -15,13 +13,19 @@ func _ready() -> void:
 	drag_component.snapped.connect(_on_note_snapped)
 	current_snapped_position = global_position
 
-
 func _on_note_started_snap() -> void:
 	last_snapped_position = global_position
-	print("Last snapp updated!")
 
 func _on_note_snapped(target_pos : Vector2) -> void:
 	current_snapped_position = target_pos
 	attempt_to_snap.emit(self)
-	print("Attempted to snap")
 	drag_component.target_pos = current_snapped_position
+
+func enter_folder() -> void:
+	var tween := create_tween()
+	
+	tween.tween_property(self, "scale", Vector2.ZERO, 0.15) \
+		.set_trans(Tween.TRANS_SINE) \
+		.set_ease(Tween.EASE_IN)
+	
+	tween.tween_callback(Callable(self, "queue_free"))
