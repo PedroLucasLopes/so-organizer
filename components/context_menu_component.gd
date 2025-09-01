7a57a153464dc1a02a9cb14e7ec646b8c59fea44
Gabriel Bonfim Silva de Moraes <78149287+asimov1239@users.noreply.github.com>
2025-09-01 15:26:01 -0300

    delete_button

--- components/context_menu_component.gd
+++ components/context_menu_component.gd
@@ -2,6 +2,8 @@ class_name ContextMenuComponent extends Node
 
 const CONTEXT_MENU = preload("res://scenes/context_menu.tscn")
 
+signal context_menu_button_pressed
+
 @export var node: Node2D
 @export var control_node : Control
 @export var area_2d: Area2D
@@ -12,8 +14,8 @@ var context_menu_active : bool = false
 var clickable : bool = false
 var context_menu : ContextMenu = null
 
-func _process(delta: float) -> void:
-	print(context_menu)
+#func _process(delta: float) -> void:
+	#print(context_menu)
 
 func _ready() -> void:
 	if area_2d:
@@ -34,10 +36,16 @@ func _on_area_mouse_exited() -> void:
 		context_menu_active = false
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and !context_menu_active and event.is_released():
+	if event is InputEventMouseButton and !context_menu_active and event.is_released() and clickable:
 		if event.button_index == MOUSE_BUTTON_RIGHT:
 			context_menu = CONTEXT_MENU.instantiate()
 			node.add_child(context_menu)
 			context_menu.global_position = node.get_global_mouse_position()
 			context_menu.reveal_context_menu(context_menu_size)
+			context_menu.context_menu_button_pressed.connect(_on_context_menu_button_pressed)
 			context_menu_active = true
+
+func _on_context_menu_button_pressed(command) -> void:
+	context_menu_button_pressed.emit(node, command)
+	context_menu_active = false
+	context_menu.queue_free()
