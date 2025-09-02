6118f5696c40f9f465393b432c42f52a51eab3d4
Gabriel Bonfim Silva de Moraes <78149287+asimov1239@users.noreply.github.com>
2025-09-01 21:52:18 -0300

    Update selection_controller.gd

--- scripts/selection_controller.gd
+++ scripts/selection_controller.gd
@@ -23,5 +23,22 @@ func _input(event: InputEvent) -> void:
 		update_selection_box()
 
 
+func update_selection_box() -> void:
+	var current_mouse_position = selection_rect.get_global_mouse_position()
+	
+	var top_left = Vector2(
+		min(start_drag_position.x, current_mouse_position.x),
+		min(start_drag_position.y, current_mouse_position.y)
+	)
+	
+	var size = Vector2(
+		abs(current_mouse_position.x - start_drag_position.x),
+		abs(current_mouse_position.y - start_drag_position.y)
+	)
+	
+	selection_rect.position = top_left
+	selection_rect.size = size
+	selection_rect.visible = true
+
 func select_icons_in_area() -> void:
 	pass
